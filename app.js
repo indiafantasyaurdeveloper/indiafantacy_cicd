4172340d2d31feea8af53c16b5e331a992d6fd93
Manish Prajapati <manish.prajapati@octalsoftware.com>
2024-07-11 13:51:07 +0530

    Autoscalling

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.get('/hello_world', (req, res) => {
     res.send('Hello World');
 })
 app.get('/', (req, res) => {
-    res.send('Octal IT Solution LLP');
+    res.send('Octal IT Solution LLP - Autoscalling');
 })
