20b4f76ae6a4dc93f36af6258a014bd978841211
Manish Prajapati <manish.prajapati@octalsoftware.com>
2024-07-11 12:54:53 +0530

    Fixes

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require('express');
 // Express Initialize
 const app = express();
 const port = 8000;
-app.listen(port,()=> {
-console.log('listen port 8000');
+app.listen(port, () => {
+    console.log('listen port 8000');
 })
 //create api
-app.get('/hello_world', (req,res)=>{
-res.send('Hello World');
+app.get('/hello_world', (req, res) => {
+    res.send('Hello World');
 })
-app.get('/', (req,res)=>{
-res.send('Manish Kumar');
+app.get('/', (req, res) => {
+    res.send('Octal IT Solution LLP');
 })
