16b528a55621518b708a304529a20d751c336dcb
indiafantasyaurdeveloper <164145762+indiafantasyaurdeveloper@users.noreply.github.com>
2024-07-17 13:57:06 +0530

    Update application_start.sh

--- application_start.sh
+++ application_start.sh
@@ -2,6 +2,7 @@
 
 # Stop all servers and start the server as a daemon
 forever stopall
+pm2 stop all
 # forever start /home/ubuntu/nodejs/app.js
 cd /home/ubuntu/nodejs
 pm2 start ecosystem.config.js
